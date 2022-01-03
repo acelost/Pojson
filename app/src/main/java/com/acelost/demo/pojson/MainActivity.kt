a479600e7f62d6c23e954a5bb8b918db9b30e0ae
Alexander Mironychev <aamironychev@yandex-team.ru>
2022-01-03 14:09:40 +0300

    - Replace jcenter repository to mavenCentral - Increment compile sdk version to 31 - Remove android compat dependency from demo - Remove anroid material dependency from demo

--- app/src/main/java/com/acelost/demo/pojson/MainActivity.kt
+++ app/src/main/java/com/acelost/demo/pojson/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.acelost.demo.pojson
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
 import com.acelost.pojson.android.json.Pojson2AndroidJSON
 import com.acelost.pojson.collection.Pojson2KotlinCollection
 import com.acelost.pojson.gson.Pojson2Gson
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
